7ea4b859e2a49161e98c261893ec6e1220528de2
Joe Macam <jdmacam@gmail.com>
2021-10-28 15:21:33 -0700

    jupyter notebooks implementation

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ RUN mkdir /code
 WORKDIR /code
 ADD . /code/
 RUN pip install -r requirements.txt
+CMD jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -1,40 +1,27 @@
-# TensorFlow and tf.keras
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import os
+import PIL
 import tensorflow as tf
 
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
 print(tf.__version__)
 
-# The Fashion MNIST dataset contains 70,000 grayscale images in 10 categories. 
-# The images show individual articles of clothing at low resolution (28 by 28 pixels),
-# as seen here  https://tensorflow.org/images/fashion-mnist-sprite.png
-fashion_mnist = tf.keras.datasets.fashion_mnist
+import pathlib
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
 
-# Here, 60,000 images are used to train the network and 10,000 images to evaluate how accurately 
-# the network learned to classify images.
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# .glob() returns an object of a list of files with the given syntax
+# convert to a list to access that list
+image_count = len(list(data_dir.glob("*/*.jpg")))
 
-# list to convert the int labels to their clothing representation
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+print (f"Number of images: {image_count}")
 
-# print the format of the dataset in the format (<number of images>, <x-resolution pixels>, <y-resolution pixels>)
-print(train_images.shape)
 
-# plt.figure()
-# plt.imshow(train_images[0])
-# plt.colorbar()
-# plt.grid(False)
-# plt.savefig('image.png')
-
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.savefig('image.png')
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[0]))
